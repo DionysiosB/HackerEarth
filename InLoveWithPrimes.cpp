2ee17e93f13acbd7182fe35f386e78e1abffcd19
Dionysios Barmpoutis <barmpoutis@gmail.com>
2015-12-03 19:53:15 -0500

    InLoveWithPrimes

---
+++ InLoveWithPrimes.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main(){
+
+    long tc; scanf("%ld\n", &tc);
+    while(tc--){
+        long n; scanf("%ld\n", &n);
+        if(n == 2){puts("Arjit");}
+        else{puts("Deepa");}
+    }
+
+    return 0;
+}
